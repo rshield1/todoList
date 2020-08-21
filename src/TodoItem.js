402d73904375aabb1eaaccae5338f4f4bc4ecfb7
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-08-21 13:45:56 -0400

    added onChange event to completed todos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 function TodoItem(props) {
-    return(
-        <div className='todo-item'>
+    return (
+        <div className="todo-item">
             <input 
-            type="checkbox" 
-            checked={props.item.completed} 
-            onChange={() => console.log("Changed")}
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
             />
             <p>{props.item.text}</p>
         </div>
-
     )
 }
 
