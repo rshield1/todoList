12273b081ba5e59106d58c45af9aaaa78f0a82d3
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-08-21 01:30:13 -0400

    added state instead of props to list

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import TodoItem from "./TodoItem"
 import todosData from './todosData'
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData    }
+  }
 
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
+  render() {
+ const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
   return (
     <div className='todoList'>
     {todoItems}
     </div>
   
   );
+
+  }
+
+ 
 }
 
 export default App;
