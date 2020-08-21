1036fdeaf434298c77253b6236c8e290753b3f26
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-08-21 01:12:16 -0400

    Added Todos dummy data for props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoItem from "./TodoItem"
+import todosData from './todosData'
 
 function App() {
   return (
---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Spend time with family",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Go to the gym",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Code",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Watch ESPN",
+        completed: false
+    }
+]
+
+export default todosData
