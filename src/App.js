616e7c3846059ac70b9a6579cbd13bf6ef4cf182
Robert Shields <34387120+rshield1@users.noreply.github.com>
2020-08-21 01:23:43 -0400

    added props to todolist

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import TodoItem from "./TodoItem"
 import todosData from './todosData'
 
 function App() {
+
+  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
   return (
     <div className='todoList'>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+    {todoItems}
     </div>
   
   );
